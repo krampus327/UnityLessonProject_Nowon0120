3e17c1df64cecc592ef2096b74b774d13de4a660
krampus327 <95688249+krampus327@users.noreply.github.com>
2022-01-25 21:59:35 +0900

    0125

---
+++ HorseRacing/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Transform tr;
+    public float distance;
+    public Vector3 dir;
+    public float minSpeed;
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = gameObject.GetComponent<Transform>();
+        RacingPlay.instance.Register(this);
+        // instance의 초기화하는 Awake 함수에서, instasnce에 접근하는 구문은 Start함수에서 일반적으로 수행한다.
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float moveSpeed = Random.Range(minSpeed, maxSpeed);
+        Vector3 moveVec = dir * moveSpeed * Time.deltaTime;
+        tr.Translate(moveVec);
+        distance += moveVec.magnitude;
+    }
+}
--- HorseRacing/Assets/Scripts/RacingPlay.cs
+++ HorseRacing/Assets/Scripts/RacingPlay.cs
@@ -4,47 +4,35 @@ using UnityEngine;
 
 public class RacingPlay : MonoBehaviour
 {
-    public List<GameObject> list_Player = new List<GameObject> (); // 플레이어를 담을 리스트
-    public float minSpeed = 0; // 랜덤속도 생성 최소값
-    public float maxSpeed = 10; // 랜덤속도 생성 최대값
-    public Transform goalLine;
-    public Transform platform1Grade;
-    public Transform platform2Grade;
-    public Transform platform3Grade;
+    #region singleton (싱글톤) 패턴
+    static public RacingPlay instance;
 
-    List<GameObject> list_Finished = new List<GameObject> (); // 끝난 플레이어를 저장할 리스트
-    bool isRacingFinished = false; // 경주 끝났는지 체크할 변수
-    private void Update()
+    private void Awake()
     {
-        // 경주가 안끝났으면 ~
-        if (isRacingFinished == false) 
-        {
-            GameObject tmpFinishedPlayer = null;
-            // 플레이어 리스트 반복
-            foreach (GameObject player in list_Player)
-            {
-                float moveSpeed = Random.Range(minSpeed, maxSpeed); // 랜덤 속도 생성
-                Transform tmpTr = player.GetComponent<Transform>(); // 현재 플레이어으 Transform 컴포넌트 가져옴
-                float moveDistance = moveSpeed * Time.deltaTime; // 속도에 델타타임 곱해서 거리계산 + 프레임수 차이에 의한 이동거리차이 없앰
-                Vector3 moveVector = Vector3.forward * moveDistance; // forward 단위벡터에 이동할 거리 곱해서 이동할 벡터 구함
-                tmpTr.Translate(moveVector); // 이동할 벡터만큼 이동하라는 명령
+        if(instance == null) instance = this;
+    }
+    #endregion
 
-                // 결승점 넘었으면 ~ 
-                if(tmpTr.position.z > goalLine.position.z)
-                {
-                    list_Finished.Add(player);
-                    tmpFinishedPlayer = player;
-                }
-            }
-            list_Player.Remove(tmpFinishedPlayer);
-            // 플레이어 5명이 모두 들어왔으면 ~ 
-            if(list_Finished.Count >= 5)
+    private List<PlayerMove> list_PlayerMove = new List<PlayerMove> ();
+    private int grade;
+
+    public void Register(PlayerMove playerMove)
+    {
+        list_PlayerMove.Add(playerMove);
+        Debug.Log($"{playerMove.gameObject.name} (이)가 등록 완료 되었습니다 현재 총 등록수 : {list_PlayerMove.Count}");
+    }
+    public Transform Get1GradePlayer()
+    {
+        Transform leader = list_PlayerMove[0].gameObject.GetComponent<Transform>();
+        float prevDistance = list_PlayerMove[0].distance;
+        foreach(PlayerMove playerMove in list_PlayerMove)
+        {
+            if(playerMove.distance > prevDistance)
             {
-                isRacingFinished = true;
-                list_Finished[0].transform.GetComponent<Transform>().position = platform1Grade.position;
-                list_Finished[1].transform.GetComponent<Transform>().position = platform2Grade.position;
-                list_Finished[2].transform.GetComponent<Transform>().position = platform3Grade.position;
+                prevDistance = playerMove.distance;
+                leader = playerMove.gameObject.GetComponent<Transform>();
             }
         }
+        return leader;
     }
 }
