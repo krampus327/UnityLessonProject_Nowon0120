5dd3a0c82e7fa20867ed246cd2f246b766ccb044
krampus327 <95688249+krampus327@users.noreply.github.com>
2022-02-16 21:55:12 +0900

    0216
    DiceGame

--- RollADice/Assets/02.Script/DicePlayManager.cs
+++ RollADice/Assets/02.Script/DicePlayManager.cs
@@ -40,6 +40,21 @@ public class DicePlayManager : MonoBehaviour
     public Text diceNumText;
     public Text goldenDiceNumText;
 
+    private int _starScore;
+    public int starScore
+    {
+        set
+        {
+            _starScore = value;
+            starScoreText.text = _starScore.ToString();
+        }
+        get
+        {
+            return _starScore;
+        }
+    }
+    public Text starScoreText;
+
     private void Awake()
     {
         if(instance == null) instance = this;
@@ -55,6 +70,15 @@ public class DicePlayManager : MonoBehaviour
         diceValueText.text = diceValue.ToString();
         MovePlayer(diceValue);
     }
+    public void RollADiceInverse()
+    {
+        if (diceNum < 1) return;
+
+        diceNum--;
+        int diceValue = Random.Range(1, 7);
+        diceValueText.text = diceValue.ToString();
+        MovePlayer(diceValue * (-1));
+    }
     public void RollAGoldenDice(int diceValue)
     {
         if (goldenDiceNum < 1) return;
@@ -62,16 +86,45 @@ public class DicePlayManager : MonoBehaviour
         goldenDiceNum--;
         MovePlayer(diceValue);
     }
+    public void RollAGoldenDiceInverse(int diceValue)
+    {
+        if (goldenDiceNum < 1) return;
+
+        goldenDiceNum--;
+        MovePlayer(diceValue * (-1));
+    }
     private void MovePlayer(int diceValue)
     {
+        int previousTileIndex = currentTileIndex;
         currentTileIndex += diceValue;
+        CheckPlayerPassedStarTile(previousTileIndex, currentTileIndex);
         if (currentTileIndex >= list_MapTile.Count)
         {
             currentTileIndex -= (list_MapTile.Count);
         }
+        if(currentTileIndex < 0)
+        {
+            currentTileIndex = list_MapTile.Count + currentTileIndex;
+        }
         Vector3 target = GetTilePosition(currentTileIndex);
         Player.instance.Move(target);
         list_MapTile[currentTileIndex].GetComponent<TileInfo>().TileEvent();
+    }
+    private void CheckPlayerPassedStarTile(int previousIndex, int currentIndex)
+    {
+        TileInfo_Star tmpStarTile = null;
+        for (int i = previousIndex + 1; i <= currentIndex; i++)
+        {
+            int tmpIndex = i;
+            if (tmpIndex >= list_MapTile.Count)
+                tmpIndex -= (list_MapTile.Count);
+
+            bool isOK = list_MapTile[tmpIndex].TryGetComponent(out tmpStarTile);
+            if (isOK)
+            {
+                starScore += tmpStarTile.starValue;
+            }
+        }
     }
     private Vector3 GetTilePosition(int tileIndex)
     {
