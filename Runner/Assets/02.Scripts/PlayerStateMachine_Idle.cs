b63b0f1fb993767d7c1cd14f9385ee361e1f8063
krampus327 <95688249+krampus327@users.noreply.github.com>
2022-04-12 21:53:12 +0900

    0412
    RPG

--- Runner/Assets/02.Scripts/PlayerStateMachine_Idle.cs
+++ Runner/Assets/02.Scripts/PlayerStateMachine_Idle.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class PlayerStateMachine_Idle : PlayerStateMachine
 {
-    override 
+    public override PlayerState UpdateState()
+    {
+        PlayerState nextPlayerState = playerState;
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Prepare:
+                animator.Play("Idle");
+                state = State.OnAction;
+                break;
+            case State.Casting:
+                break;
+            case State.OnAction:
+                break;
+            case State.Finish:
+                break;
+            default:
+                break;
+        }
+        return nextPlayerState;
+    }
 }
