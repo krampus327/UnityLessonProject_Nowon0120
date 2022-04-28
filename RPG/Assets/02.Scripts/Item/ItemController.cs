482b4b6d2082b1334b8530c2a3798c1398367f62
krampus327 <95688249+krampus327@users.noreply.github.com>
2022-04-28 21:57:44 +0900

    0428
    RPG
    Error

--- RPG/Assets/02.Scripts/Item/ItemController.cs
+++ RPG/Assets/02.Scripts/Item/ItemController.cs
@@ -15,7 +15,7 @@ public class ItemController : MonoBehaviour
     [Header("Dropping Effect")]
     public float popForce = 1f;
     public float rotateSpeed = 1f;
-      
+
     [Header("Kinematics")]
     private Rigidbody rb;
     private BoxCollider col;
@@ -25,14 +25,18 @@ public class ItemController : MonoBehaviour
     private Vector3 rendererOffset;
     private float elapsedFixedTime;
     private Vector3 eulerAngleOrigin;
+    private Coroutine coroutine = null;
     //====================================================================
     //************************** Public Methods **************************
     //====================================================================
 
-    public void PickUp()
+    public void PickUp(Player player)
     {
-        // to do -> 인벤토리에 아이템 추가
-        // to do -> 픽업 효과
+        if (coroutine == null)
+        {
+            // to do -> 인벤토리에 아이템 추가
+            coroutine = StartCoroutine(E_PickUpEffect(player));
+        }
     }
 
     //====================================================================
@@ -72,9 +76,11 @@ public class ItemController : MonoBehaviour
     {
         rb.velocity = Vector3.zero;
         rb.AddForce(Vector3.up * popForce, ForceMode.Impulse);
+
+        Debug.Log("Imma rotating");
         while (doFloatingEffect == false)
         {
-            Collider[] grounds = Physics.OverlapSphere(rb.position - new Vector3(0f, col.size.y, 0f), 0.3f, groundLayer);
+            Collider[] grounds = Physics.OverlapSphere(rb.position - new Vector3(0f, col.size.y / 2, 0f), 0.1f, groundLayer);
 
             if (grounds.Length > 0)
                 doFloatingEffect = true;
@@ -83,6 +89,58 @@ public class ItemController : MonoBehaviour
             yield return null;
         }
         rendererTransform.eulerAngles = eulerAngleOrigin;
+    }
+
+    IEnumerator E_PickUpEffect(Player player)
+    {
+        doFloatingEffect = false;
+        rb.velocity = Vector3.zero;
+        //rb.AddForce(Vector3.up * popForce, ForceMode.VelocityChange);
+
+        rb.useGravity = false;
+        bool isReachedToPlayer = false;
+        CharacterController playerCol = player.GetComponent<CharacterController>();
+        Vector3 playerOffSet = new Vector3(0, playerCol.height / 2 + playerCol.radius, 0);
+
+        MeshRenderer meshRenderer = rendererTransform.GetComponent<MeshRenderer>();
+        float fadeAlpha = 1f;
+        Color fadeColor = meshRenderer.material.color;
+
+        float pickUpTimer = 10f;
+
+        rb.velocity = Vector3.zero;
+        while (pickUpTimer > 0 && isReachedToPlayer == false)
+        {
+            // 아이템과 플레이어 사이 거리
+            float distance = (Vector3.Distance(player.transform.position + playerOffSet, rb.position));
+
+            // 아이템이 플레이어게 도달함
+            if (distance < 0.1f)
+            {
+                isReachedToPlayer = true;
+                rb.velocity = Vector3.zero;
+                break;
+            }
+
+            // 아이템 -> 플레이어 로 날아갈 속도 벡터
+            Vector3 moveVec = player.transform.position - rb.position;
+
+            Debug.Log(moveVec);
+            // 날아가자
+            //transform.position += moveVec * Time.deltaTime; // 단순 transform 포지션 변경, rb 포지션과 속도 연산 추가 필요
+            //rb.position += moveVec * Time.deltaTime; // 단순 rb 포지션 변경
+            rb.MovePosition(rb.position + moveVec * Time.deltaTime); // 선형보간하면서 rb 포지션 변경
+
+            // 색변경
+            fadeAlpha -= Time.deltaTime;
+            fadeColor = new Color(fadeColor.r, fadeColor.g, fadeColor.b, fadeAlpha);
+            meshRenderer.material.color = fadeColor;
+
+            pickUpTimer -= Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 
 }
